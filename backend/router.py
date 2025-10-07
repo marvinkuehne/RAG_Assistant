d467a88503b4fef058c18c50eef26910178003d1
Marvin Kühne <marvin.kuehne32@gmail.com>
2025-10-07 12:15:30 +0200

    Allowed all origins

--- backend/router.py
+++ backend/router.py
@@ -30,7 +30,7 @@ origins = [
 # Block unauthorized requrests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
