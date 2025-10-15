75f5136b064282dbb368bde2d65c27a1ad2e0cfd
Marvin Kühne <marvin.kuehne32@gmail.com>
2025-10-15 15:11:10 +0200

    last try

--- backend/router.py
+++ backend/router.py
@@ -6,13 +6,16 @@ import mimetypes
 
 from starlette.middleware.cors import CORSMiddleware
 
-from backend.Loader import load_documents, split_documents, get_embedding, create_ids, add_to_chroma, get_user_chroma_dir, get_vectorstore
-from backend.db.chat_db import create_session, get_sessions, save_session, load_session, conn
-from backend.services.chroma_service import update_category
-from backend.query_data import query_rag
+from Loader import load_documents, split_documents, get_embedding, create_ids, add_to_chroma, get_user_chroma_dir, get_vectorstore
+from db.chat_db import create_session, get_sessions, save_session, load_session, conn
+from services.chroma_service import update_category
+from query_data import query_rag
 from pydantic import BaseModel
 from typing import List, Optional
 
+import sys, os
+print("CWD:", os.getcwd())
+print("sys.path:", sys.path)
 
 
 # Upload folder for upload_files request
